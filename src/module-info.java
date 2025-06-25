e9231c758a99e3f928b27e008080a8a2100c193a
Nai2203 <hnaidelyn19@gmail.com>
2025-06-24 23:20:47 -0500

    Interfaz lista
    Interfaz terminada, grafica se ve con los nuemeros

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module SistemaTransitoInteligente_BeliNai {
 	requires javafx.controls;
 	requires com.fasterxml.jackson.databind;
 	requires com.fasterxml.jackson.datatype.jsr310;
+	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
